816c0fac0f893b34a7528c6891d8117f01dbc4f1
Nguyen Duy Dao <duydao10@gmail.com>
2024-09-24 17:22:10 +0700

    checout to pp

--- client/checkout.php
+++ client/checkout.php
@@ -1,3 +1,145 @@
+
+<?php
+include_once './config/utils.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["amount"])) {
+    //get post
+    //--------sen 2 post : post order + order item ---------------
+    $name = $_POST['name'];
+$status = "pending";
+$invoice = "Site1-5555" . rand(1, 1000);
+$currency_code = "USD";
+$amount = $_POST['amount'];
+$return_url = "";
+$cancel_url = "";
+$notify_url = "";
+$address = $_POST['address'];
+$email = $_POST['email'];
+$phone = $_POST['night_phone'];
+  // send post api orders 
+    $apiUrlOrder = "http://localhost:1337/api/orders";
+
+    $data_order = [
+        "data" => [
+            "custom_name" => $name,
+            "status" => $status,
+            "invoice" => $invoice,
+            "currency_code" => $currency_code,
+            "amount" => $amount,
+            "return_url" => $return_url,
+            "cancel_url" => $cancel_url,
+            "notify_url" => $notify_url,
+            "address" => $address,
+            "email" => $email,
+            "phone" => $phone
+        ]
+    ];
+
+         $jsonDataOrder = json_encode($data_order);
+
+    $data_order =  handleCallAPI($apiUrlOrder, "POST", $jsonDataOrder);
+        //  die(print_r($data['data']['attributes']['custom_name'],true));
+        $order_id = $data_order['data']['id'];
+
+// send post api orders Item
+$quantities = [];
+$amounts = [];
+$products_id = [];
+foreach ($_POST as $key => $value) {
+  
+    if (strpos($key, 'quantity_') === 0) {
+        $quantities[] = $value; 
+    }
+
+    if (strpos($key, 'amount_') === 0) {
+        $amounts[] = $value; 
+    }
+    if (strpos($key, 'product_id_') === 0) {
+        $products_id[] = $value; 
+    }
+}
+
+
+if (count($quantities) > 0 && count($amounts) > 0) {
+
+    for ($i = 0; $i < count($quantities); $i++) {
+        $quantity = $quantities[$i];
+        $amount_item = $amounts[$i];
+        $product_id = $products_id[$i];
+        $apiUrlOrderItem = "http://localhost:1337/api/order-items";
+        
+       
+        $data_order_item = [
+            "data" => [
+                "quantity" => $quantity,
+                "amount_item" => $amount_item,
+                "order" => $order_id,
+                "product" => $product_id
+            ]
+        ];
+        $jsonDataOrderItem = json_encode($data_order_item);
+       
+        $response = handleCallAPI($apiUrlOrderItem, "POST", $jsonDataOrderItem);
+
+    }
+
+}
+        
+    function encodeFullBase64($input) {
+        // Convert binary data to hexadecimal representation
+        $hexEncoded = bin2hex($input);
+
+        // Reverse the string
+        $reversedHex = strrev($hexEncoded);
+
+        return $reversedHex;
+    }
+
+    $params = [
+        'cmd' => "_xclick",
+        'business' => "",
+        'currency_code' => $currency_code,
+        'return' => $return_url,
+        'cancel_return' => $cancel_url,
+        'notify_url' => $notify_url,
+        'invoice' =>$invoice,
+        'custom'           => json_encode(array(
+            'data' => $order_id,
+        )),
+        'full_name' => $name,
+        'address' => $address,
+        'email' => $email,
+        'night_phone' => $phone,
+        'no_shipping' => "0",
+        'amount' => $amount,
+        'order_id' => $order_id, 
+        'item_name' => "Item - {$order_id}",
+        'bn' => "PP-BuyNowBF:btn_buynow_LG.gif:NonHostedGuest",
+        
+
+        'ip_address' => "",
+        'secretKey' => $_POST['secretKey'],
+        'is_sandbox' => "yes"
+    ];
+    foreach ($quantities as $index => $quantity) {
+        $params["quantity_" . ($index + 1)] = $quantity;
+        $params["amount_" . ($index + 1)] = $amounts[$index]; 
+    }
+
+    // Chuyển đổi mảng thành chuỗi truy vấn
+    $queryString = http_build_query($params);
+
+    // Mã hóa chuỗi truy vấn
+    $encodedParams = encodeFullBase64('https://www.sandbox.paypal.com/cgi-bin/webscr?'.$queryString);
+
+    // Tạo URL với tham số ?data=
+    $url = "https://pp.premiumdodo.com/redirect.php?data=" . urlencode($encodedParams);
+    
+    // Chuyển hướng đến URL
+    header("Location: " . $url);
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
 
@@ -41,13 +183,14 @@
     <?php
     include "./components/header.php"
     ?>
+     
     <!-- End Header Area -->
     <!--================Checkout Area =================-->
     <section style="padding: 130px 0;" class="checkout_area section_gap">
         <div class="container">
 
             <div class="billing_details">
-                <form class="row contact_form" action="https://pp.premiumdodo.com/redirect.php" method="get" novalidate="novalidate">
+                <form class="row contact_form" action="" method="post" >
                     <div class="col-lg-6">
                         <h3>Billing Details</h3>
 
@@ -58,28 +201,22 @@
 
 
                         <div class="col-md-6 form-group p_star">
-                            <input placeholder="Phone number" type="text" class="form-control" id="number" name="number">
+                            <input placeholder="Phone number" type="text" class="form-control" id="night_phone" name="night_phone">
                         </div>
                         <div class="col-md-6 form-group p_star">
                             <input placeholder="Email Address" type="text" class="form-control" id="email" name="email">
                         </div>
 
-                        <div class="col-md-12 form-group p_star">
-                            <input type="text" class="form-control" id="add" name="add">
-                        </div>
 
                         <div class="col-md-12 form-group p_star">
-                            <input type="text" class="form-control" id="city" name="city">
-                            <span class="placeholder" data-placeholder="Town/City"></span>
+                            <input type="text" class="form-control" id="address" name="address">
+                            <span class="placeholder" data-placeholder="address"></span>
                         </div>
                         <div class="col-md-12 form-group p_star">
-                            <input type="text" value="Wg7Wh3F4Amoe3M7T" class="form-control" id="city" name="secretKey">
+                            <input type="text" value="Wg7Wh3F4Amoe3M7T" class="form-control" id="secretKey" name="secretKey">
                         </div>
 
 
-                        <div class="col-md-12 form-group">
-                            <textarea class="form-control" name="message" id="message" rows="1" placeholder="Order Notes"></textarea>
-                        </div>
 
                     </div>
                     <div class="col-lg-6">
@@ -94,7 +231,7 @@
                                     <span class="col-lg-3">Total</span>
                                 </li>
                                 <?php
-                                include "./config/utils.php";
+                           
                                 $total = 0;
                                 if (isset($_COOKIE['cart']) && !empty($_COOKIE['cart'])) {
                                     $cart = json_decode($_COOKIE['cart'], true);
@@ -114,7 +251,7 @@
                                             return in_array($product['id'], $productIds);
                                         });
 
-                                        foreach ($filteredProducts as $product) {
+                                        foreach ($filteredProducts as $index => $product) {
                                             $attributes = $product['attributes'];
                                             $id = (int) $product['id'];
                                             $name = $attributes['name'];
@@ -128,8 +265,11 @@
                                                         -webkit-box-orient: vertical;  overflow: hidden; font-weight: 500;" class="col-lg-6 ">' . $name . '
                                                     </span>
                                                     <span class="col-lg-3">' . $quantity . '</span>
-                                                    <span class="col-lg-3">' . $total . '</span>
+                                                    <span class="col-lg-3">' . $totalPerProduct . '</span>
                                                 </li>';
+                                                echo '<input type="hidden" name="quantity_' . ($index + 1) . '" value="' . $quantity . '">';
+                                                echo '<input type="hidden" name="amount_' . ($index + 1) . '" value="' . $price . '">';
+                                                echo '<input type="hidden" name="product_id_'. ($index + 1)  . '" value="' .  $id . '">';
                                         }
                                     }
                                 }
@@ -142,6 +282,8 @@
                                     </span>
                                     <span class="col-lg-3"></span>
                                     <span style="font-weight: 700; font-size: 16px;" class="col-lg-3"><?php echo $total ?></span>
+                                    <?php echo '<input type="hidden" name="amount' . '" value="' . $total . '">';
+                                    ?>
                                 </li>
                             </ul>
 
@@ -160,6 +302,7 @@
                                 <label for="f-option4">I’ve read and accept the </label>
                                 <a href="#">terms & conditions*</a>
                             </div>
+                         
                             <button type="submit" class="primary-btn" href="#">Proceed to Paypal</button>
                         </div>
                     </div>
@@ -167,6 +310,7 @@
             </div>
         </div>
     </section>
+    
     <!--================End Checkout Area =================-->
 
     <!-- start footer Area -->
@@ -174,7 +318,7 @@
     include "./components/footer.php"
     ?>
     <!-- End footer Area -->
-
+  
 
     <script src="js/vendor/jquery-2.2.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
--- client/insertData.php
+++ client/insertData.php
@@ -1,6 +1,6 @@
 <?php
 // Đọc file JSON
-$jsonFilePath = 'C:\xampp\htdocs\ecommerce_strpi\client\data\sample_products_2_100000.json';
+$jsonFilePath = 'C:\xampp\htdocs\ecommerce_strpi\client_strapi\client\data\sample_products_1_100000.json';
 
 $jsonData = file_get_contents($jsonFilePath);
 $dataArray = json_decode($jsonData, true);
@@ -12,7 +12,6 @@ $api_url = 'http://localhost:1337/api/products';
 function callAPI($product)
 {
     global $api_url;
-
     $data = [
         "data" => [
             "name" => $product['Title'],
@@ -20,7 +19,7 @@ function callAPI($product)
             "description" => $product['Description'],
             "price" => (float)$product['Price'],
             "discount" => ((int)$product['Sale_Price'] / (int)$product['Price']) * 100,
-            "image" => "http://localhost:1337/uploads/LEVOIT_Cordless_Vacuum_Cleaner_Stick_Vac_with_Tangle_Resistant_Design_Up_to_50_Minutes_Powerful_Suction_Rechargeable_Lightweight_and_Versatile_for_Carpet_Hard_Floor_Pet_Hair_Black_White_9d22b0bddf.jpg",
+            "image" => "http://localhost:1337/uploads/download_0360026fc0.jpg",
             "category" => 4,
             "status" => 1,
             "details" => "Chi tiết sản phẩm",
